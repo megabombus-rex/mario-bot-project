e08f8aac38e682827f30a26d23982384899b739a
JANC_PC\czech <czechumennakompie@gmail.com>
2025-05-31 12:12:26 +0200

    corrected innovation db and mutations

--- src/main.py
+++ src/main.py
@@ -49,7 +49,7 @@ def main(seed, ai_model):
 
 if __name__ == "__main__":
     common_rates = CommonRates(0.8, 0.1, 0.4, 0.2, 0.6, 5)
-    innovation_db = InnovationDatabase()
+    innovation_db = InnovationDatabase(11) # (input size + output size -1) is the beginning node count
     model = Model.generate_network(input_size=6, output_size=6, common_rates=common_rates, innovation_db=innovation_db, seed=DEFAULT_SEED_MODEL)
     #input = InputData(10, 20, 25, 30, 40, 50)
     #model(input=input)
